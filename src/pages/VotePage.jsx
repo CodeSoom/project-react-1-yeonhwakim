c73096b3853bc52ed52b7a25981258ff9d283472
yeonhwa <yeonhwa6717@gmail.com>
2020-08-11 22:04:56 +0900

    파일 분리
    - pages 폴더 생성
    - containers 폴더 생성
    - components 폴더 생성

--- src/pages/VotePage.jsx
+++ src/pages/VotePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import VoteContainer from './VoteContainer';
+import VoteContainer from '../containers/VoteContainer';
 
 export default function VotePage() {
   return (
