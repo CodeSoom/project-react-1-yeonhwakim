e198727b3fa42d43248a91b210040ffd3dc2abfb
hannut91 <hannut91@gmail.com>
2020-08-11 22:56:40 +0900

    Refactor App component
    Move loadItem in useEffect life cycle function because this is more than
    resonable.

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,15 @@ import { loadItem, saveItem } from './services/storage';
 
 export default function App() {
   const dispatch = useDispatch();
-  const accessToken = loadItem('accessToken');
-
-  if (!accessToken) {
-    saveItem(accessToken, 'user1');
-  }
 
   useEffect(() => {
-    dispatch(loadUser(loadItem('accessToken')));
+    const accessToken = loadItem('accessToken');
+    // TODO: Delete in production
+    if (!accessToken) {
+      saveItem(accessToken, 'user1');
+    }
+
+    dispatch(loadUser(accessToken));
   }, []);
 
   return (
