000709035975fc5a0fc89a305a226ed8913ed654
yeonhwa <yeonhwa6717@gmail.com>
2020-08-11 22:43:22 +0900

    탭 페이지 추가
    - TabPage.test.jsx 추가 탭이름 랜더링 테스트
    - TabPage.jsx 추가

---
+++ src/pages/TabPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+export default function TabPage() {
+  return (
+    <>
+      <Link to="/vote">VOTE</Link>
+      <Link to="/menu">MENU</Link>
+    </>
+  );
+}
