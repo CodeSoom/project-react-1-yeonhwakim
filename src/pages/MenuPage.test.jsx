6b809d9f2ee563ad5c623a07341ef4cb615078f0
yeonhwa <yeonhwa6717@gmail.com>
2020-08-13 00:59:42 +0900

    MenuPage.test.jsx 테스트 수정
    - loadMenuList오류 수정

--- src/pages/MenuPage.test.jsx
+++ src/pages/MenuPage.test.jsx
@@ -2,14 +2,35 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 import MenuPage from './MenuPage';
 
 describe('MenuPage', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+
+    useDispatch.mockImplementation(() => dispatch);
+
+    useSelector.mockImplementation((selector) => selector({
+      menuList: [],
+    }));
+  });
+
   it('render menu title', () => {
     const { container } = render((
       <MenuPage />
     ));
 
     expect(container).toHaveTextContent('Menu!!!!');
+  });
+
+  it('loads vote list', () => {
+    render((
+      <MenuPage />
+    ));
+    expect(dispatch).toBeCalled();
   });
 });
