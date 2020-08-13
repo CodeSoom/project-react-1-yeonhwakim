dc5544959e06a7f6e8dffcd8a3330609fc243cae
yeonhwa <yeonhwa6717@gmail.com>
2020-08-14 00:52:09 +0900

    MenuForm input onChange event 테스트 추가
    - MenuForm.test.jsx onChange event 테스트 추가
    - MenuForm.jsx input 태그에 onChange event 추가

--- src/components/MenuForm.jsx
+++ src/components/MenuForm.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
-export default function MenuForm({ field }) {
+export default function MenuForm({ field, onChange }) {
+  function handleChange(event) {
+    const { target: { value } } = event;
+    onChange({ value });
+  }
+
   return (
     <>
       <form>
-        <input type="text" placeholder="menu" value={field} />
+        <input
+          type="text"
+          placeholder="menu"
+          value={field}
+          onChange={handleChange}
+        />
         <button type="submit">추가</button>
       </form>
     </>
--- src/components/MenuForm.test.jsx
+++ src/components/MenuForm.test.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import MenuForm from './MenuForm';
 
 describe('MenuForm', () => {
+  const handleChange = jest.fn();
+
+  beforeEach(() => {
+    handleChange.mockClear();
+  });
+
   function renderMenuForm(newMenu) {
     return render((
       <MenuForm
         field={newMenu}
+        onChange={handleChange}
       />
     ));
   }
@@ -21,6 +28,20 @@ describe('MenuForm', () => {
     const control = { placeholder: 'menu', value: newMenu };
 
     const input = getByPlaceholderText(control.placeholder);
+
     expect(input.value).toBe(control.value);
+  });
+
+  it('listens change events', () => {
+    const { getByPlaceholderText } = renderMenuForm();
+
+    const control = { placeholder: 'menu', value: '김밥천국' };
+    const { value } = control;
+
+    const input = getByPlaceholderText(control.placeholder);
+
+    fireEvent.change(input, { target: { value } });
+
+    expect(handleChange).toBeCalledWith({ value });
   });
 });
