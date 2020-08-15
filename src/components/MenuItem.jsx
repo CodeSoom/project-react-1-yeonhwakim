c076609d00635ad63de90007128c7ae5c105a739
yeonhwa <yeonhwa6717@gmail.com>
2020-08-15 13:15:22 +0900

    MenuItem.jsx에 blur 이벤트 추가
    - MenuItem.test.jsx에 blur이벤트 추가
    - MenuItem.jsx에 updateId와 updateName을 받는 blur이벤트 추가

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
-export default function VoteItem({ menuItem: { id, name }, onClick }) {
+export default function VoteItem({ menuItem: { id, name }, onClick, onBlur }) {
+  function handleBlur(event, updateId) {
+    const { target: { textContent } } = event;
+    onBlur({
+      updateId,
+      updateName: textContent,
+    });
+  }
+
   return (
     <>
       <li>
-        <span contentEditable>{name}</span>
+        <span
+          contentEditable
+          suppressContentEditableWarning
+          onBlur={(event) => handleBlur(event, id)}
+        >
+          {name}
+
+        </span>
         <button
           type="button"
           data-testid={id}
