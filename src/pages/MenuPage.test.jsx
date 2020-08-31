dffd4bc9337e6ff01a539d2d60107f9084d70b9f
yeonhwa <yeonhwa6717@gmail.com>
2020-08-31 23:44:09 +0900

    MenuPage 수정
    - params에서 얻은 homeId와 roomId MenuContainer 컴포넌트로 넘겨준다.
    - params가 있을 때랑 없을 때 테스트 추가

--- src/pages/MenuPage.test.jsx
+++ src/pages/MenuPage.test.jsx
@@ -4,7 +4,11 @@ import { render } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import MenuPage from './MenuPage';
+
+import HOME from '../../fixtures/home';
 
 describe('MenuPage', () => {
   const dispatch = jest.fn();
@@ -15,22 +19,47 @@ describe('MenuPage', () => {
     useDispatch.mockImplementation(() => dispatch);
 
     useSelector.mockImplementation((selector) => selector({
-      menuList: [],
+      menuList: given.menuList,
     }));
   });
 
-  it('render menu title', () => {
-    const { container } = render((
-      <MenuPage />
-    ));
+  context('with params props', () => {
+    given('menuList', () => (HOME[0].room[0].menu));
 
-    expect(container).toHaveTextContent('Menu!!!!');
+    it('renders menuList', () => {
+      const params = { homeId: 1, roomId: 1 };
+
+      const { container } = render(
+        <MemoryRouter>
+          <MenuPage params={params} />
+        </MemoryRouter>,
+      );
+
+      HOME[0].room.filter((roomItem) => (
+        roomItem.id === params.roomId
+      ))[0].menu.map((menuItem) => (
+        expect(container).toHaveTextContent(menuItem.name)
+      ));
+    });
   });
 
-  it('loads vote list', () => {
-    render((
-      <MenuPage />
-    ));
-    expect(dispatch).toBeCalled();
+  context('without params props', () => {
+    given('menuList', () => (HOME[0].room[0].menu));
+
+    it('renders name', () => {
+      const homeId = HOME[0].id;
+      const roomId = HOME[0].room[0].id;
+      const { container } = render(
+        <MemoryRouter initialEntries={[`/home/${homeId}/room/${roomId}/menu`]}>
+          <MenuPage />
+        </MemoryRouter>,
+      );
+
+      HOME[0].room.filter((roomItem) => (
+        roomItem.id === roomId
+      ))[0].menu.map((menuItem) => (
+        expect(container).toHaveTextContent(menuItem.name)
+      ));
+    });
   });
 });
